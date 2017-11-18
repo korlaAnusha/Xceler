7c964e490ca3eae074f1be148853b157069bc3e2
anushakorla <anusha@rubyeffect.com>
2017-11-18 16:08:15 +0530

    Implemented Sign in and sign up feature with user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates_presence_of :full_name
+  validates_presence_of :age
+  validates_numericality_of :age
+end
---
+++ db/migrate/20171118095949_add_column_to_users.rb
@@ -0,0 +1,6 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :full_name, :string
+    add_column :users, :age, :integer
+  end
+end
